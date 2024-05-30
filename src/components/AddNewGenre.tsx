24a29c0578452b498c0c5f418d391b0e90378628
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-30 12:33:56 +0100

    chore: Add AddNewGenre component for adding new genres

---
+++ src/components/AddNewGenre.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useCreateGenre } from "../hooks/useCreateGenre";
+
+function AddNewGenre() {
+  const [newGenre, setNewGenre] = useState("");
+  const { mutate, isPending } = useCreateGenre();
+
+  function handleAddGenre(e: React.FormEvent, newGenre: string) {
+    e.preventDefault();
+    mutate(newGenre);
+    setNewGenre("");
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Add new genre"
+        value={newGenre}
+        onChange={(e) => setNewGenre(e.target.value)}
+      />
+      <button
+        disabled={isPending || !newGenre}
+        type="submit"
+        onClick={(e) => handleAddGenre(e, newGenre)}
+      >
+        Add new Genre
+      </button>
+    </form>
+  );
+}
+
+export default AddNewGenre;
