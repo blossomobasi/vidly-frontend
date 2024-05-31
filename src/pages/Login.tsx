9867b6ae332bc7355d3feef8d7d02657eecd8cd7
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-31 12:25:51 +0100

    feat: Add login and signup functionality to Login and SignUp components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,63 @@
 import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
+import { useState } from "react";
+import { useLogin } from "../hooks/auth/useLogin";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate: login, isPending, error } = useLogin();
+
+  function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    if (!email || !password) {
+      console.log("Please fill in all fields");
+      return;
+    }
+
+    if (error) {
+      console.log(error.message);
+    }
+    login({ email, password });
+  }
+
   return (
     <div className={styles.loginFormContainer}>
-      <form className={styles.loginForm}>
+      <form className={styles.loginForm} onSubmit={handleLogin}>
         <h2 className={styles.title}>Login</h2>
         <div className={styles.formGroup}>
           <label htmlFor="email" className={styles.label}>
             Email:
           </label>
-          <input type="email" id="email" className={styles.input} required />
+          <input
+            disabled={isPending}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+            className={styles.input}
+            required
+          />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="password" className={styles.label}>
             Password:
           </label>
           <input
+            disabled={isPending}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             id="password"
             className={styles.input}
             required
           />
         </div>
-        <button type="submit" className={styles.button}>
+        <button
+          disabled={isPending || !email || !password}
+          type="submit"
+          className={styles.button}
+        >
           Login
         </button>
         <p className={styles.signup}>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,21 +1,71 @@
 import { Link } from "react-router-dom";
 import styles from "./SignUp.module.css";
+import { useState } from "react";
+import { useCreateUser } from "../hooks/auth/useCreateUser";
 function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const { mutate: creatingUser, isPending, error } = useCreateUser();
+
+  function handleRegisterUser(e: React.FormEvent) {
+    e.preventDefault();
+    if (!name || !email || !password || !confirmPassword) {
+      console.log("Please fill in all fields");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      console.log("Passwords do not match");
+      return;
+    }
+    if (error) {
+      console.log(error.message);
+    }
+    creatingUser({ name, email, password });
+  }
+
   return (
     <div className={styles.signupFormContainer}>
-      <form className={styles.signupForm}>
+      <form className={styles.signupForm} onSubmit={handleRegisterUser}>
         <h2 className={styles.title}>Sign Up</h2>
         <div className={styles.formGroup}>
+          {/* Name */}
+          <label htmlFor="name" className={styles.label}>
+            Name:
+          </label>
+          <input
+            disabled={isPending}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            id="name"
+            className={styles.input}
+            required
+          />
+
           <label htmlFor="email" className={styles.label}>
             Email:
           </label>
-          <input type="email" id="email" className={styles.input} required />
+          <input
+            disabled={isPending}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+            className={styles.input}
+            required
+          />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="password" className={styles.label}>
             Password:
           </label>
           <input
+            disabled={isPending}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             id="password"
             className={styles.input}
@@ -27,13 +77,22 @@ function SignUp() {
             Confirm Password:
           </label>
           <input
+            disabled={isPending}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             type="password"
             id="confirmPassword"
             className={styles.input}
             required
           />
         </div>
-        <button type="submit" className={styles.button}>
+        <button
+          disabled={
+            isPending || !name || !email || !password || !confirmPassword
+          }
+          type="submit"
+          className={styles.button}
+        >
           Sign Up
         </button>
 
