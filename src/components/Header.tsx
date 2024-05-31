b557dbdb7693e0deb98e3c647e54e9f66f9cb93f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-31 08:04:51 +0100

    feat: Add navigation buttons for signup and login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const navigate = useNavigate();
   const links = [
     { name: "home", href: "/" },
+    { name: "genre", href: "/genres" },
     { name: "about", href: "/about" },
   ];
 
   return (
     <nav className={styles.nav}>
+      <div>
+        <button onClick={() => navigate("signup")}>Signup</button>
+        <button onClick={() => navigate("login")}>login</button>
+      </div>
       <ul>
         {links.map((link) => (
           <NavLink key={link.name} to={link.href}>
