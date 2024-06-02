339cf768b887ab571490b04b810f931aeaf358da
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-02 22:45:40 +0100

    refactor: Update PageNotFound component to use react-router-dom's useNavigate hook for navigation

--- src/ui/PageNotFound.tsx
+++ src/ui/PageNotFound.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Page not found</h1>
-      <button onClick={() => history.back()}>Go back</button>
+      <button onClick={() => navigate(-1)}>Go back</button>
     </div>
   );
 };
