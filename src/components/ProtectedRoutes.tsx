017dc5e0fbb1d0eb32edf52c0247c2783b1f9505
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-02 22:34:31 +0100

    refactor: Add authentication check to ProtectedRoutes component

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../hooks/auth/useUser";
+import { useEffect } from "react";
+
 function ProtectedRoutes({ children }: { children: React.ReactNode }) {
-  return children;
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, navigate, isLoading]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoutes;
