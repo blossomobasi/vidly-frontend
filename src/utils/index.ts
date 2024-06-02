f234913b1d0e9bbee6b78c93321ccfe631c9630b
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-02 22:38:08 +0100

    refactor: Update getTokenFromHeaders to getToken from localStorage and decode token using jwt-decode library

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,16 @@
-export function getTokenFromHeaders(headers: Headers) {
-  const token = headers.get("x-auth-token");
-  if (!token) throw new Error("No token found in headers");
+import { jwtDecode } from "jwt-decode";
 
+export function decodeToken(token: string) {
+  if (!token) return null;
+  try {
+    return jwtDecode(token);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export function getToken() {
+  const token = localStorage.getItem("token");
   return token;
 }
