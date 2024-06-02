8fd91edf5e11e6d745d1ee3ba29cee5f386b0d08
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-02 22:36:16 +0100

    refactor: Add getCurrentUser function for retrieving user information

---
+++ src/services/auth/getCurrentUser.ts
@@ -0,0 +1,20 @@
+import { getToken } from "../../utils";
+
+export async function getCurrentUser() {
+  const token = getToken();
+  if (!token) throw new Error("No token found. Please login again.");
+
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_BASE_URL}/api/users/me`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "x-auth-token": token,
+      },
+    }
+  );
+
+  if (!res.ok) throw new Error("Failed to get current user");
+
+  return res.json();
+}
