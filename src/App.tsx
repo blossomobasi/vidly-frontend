600b13744ab65b9a61f2007ed260b24ed25000b9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-03 14:57:27 +0100

    refactor: Add admin routes and protected routes for genre management

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,15 @@ import GenresPage from "./pages/GenresPage";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./ui/PageNotFound";
 import ProtectedRoutes from "./components/ProtectedRoutes";
+import AdminProtectedRoute from "./components/AdminProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import GenreIdPage from "./pages/GenreIdPage";
 
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 1000 * 60, // 1 minute
+        // staleTime: 1000 * 60, // 1 minute
       },
     },
   });
@@ -33,12 +36,21 @@ function App() {
               </ProtectedRoutes>
             }
           >
+            <Route
+              path="admin-route"
+              element={
+                <AdminProtectedRoute>
+                  <AdminRoute />
+                </AdminProtectedRoute>
+              }
+            />
+            <Route path="dashboard" element={<Dashboard />} />
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="genres" element={<GenresPage />} />
+            <Route path="genres/:genreId" element={<GenreIdPage />} />
           </Route>
 
-          <Route path="dashboard" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
 
