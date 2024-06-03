5ce2029026976bceedd6c3fec4ad0988b69c15aa
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-03 15:00:40 +0100

    refactor: Add dashboard link to Header component for authenticated users and logout function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 import { useUser } from "../hooks/auth/useUser";
+import { useLogout } from "../hooks/auth/useLogout";
 
 const Header = () => {
+  const logout = useLogout();
   const navigate = useNavigate();
-  const { user, isAuthenticated } = useUser();
+  const { user, isAuthenticated, isAdmin } = useUser();
   const links = [
     { name: "home", href: "/" },
     { name: "genre", href: "/genres" },
     { name: "about", href: "/about" },
+    { name: "dashboard", href: "dashboard" },
   ];
+
+  function handleLogout() {
+    logout();
+  }
 
   return (
     <nav className={styles.nav}>
@@ -29,7 +36,9 @@ const Header = () => {
             {link.name}
           </NavLink>
         ))}
+        {isAdmin && <NavLink to="admin-route">Admin</NavLink>}
       </ul>
+      <button onClick={handleLogout}>Logout</button>
     </nav>
   );
 };
