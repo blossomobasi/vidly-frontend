6c9ad992989e22eab13330c8fc1b877d15d44ded
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-04 07:27:08 +0100

    refactor: Update useGenres hook to handle error with Error type

--- src/hooks/genres/useGenres.ts
+++ src/hooks/genres/useGenres.ts
@@ -3,7 +3,7 @@ import { getGenres } from "../../services/genres/getGenres";
 import { Genre } from "../../types/genre";
 
 export function useGenres() {
-  const { data, isLoading, error } = useQuery<Genre[]>({
+  const { data, isLoading, error } = useQuery<Genre[], Error>({
     queryKey: ["genres"],
     queryFn: getGenres,
   });
