251f62a03335b8815c1e0eba9cc485b303d4b3e2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-04 13:31:02 +0100

    refactor: Simplify Genres component by removing unnecessary code

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,9 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import { useGenres } from "../hooks/genres/useGenres";
 import { Genre } from "../types/genre";
 
 function Genres() {
-  const navigate = useNavigate();
   const { data: genres, isLoading, error } = useGenres();
 
   return (
@@ -11,12 +9,7 @@ function Genres() {
       {isLoading && <div>Loading...</div>}
       {error && <div>{error.message}</div>}
       {genres?.map((genre: Genre) => (
-        <button
-          onClick={() => navigate(`/genres/${genre._id}`)}
-          key={genre._id}
-        >
-          {genre.name}
-        </button>
+        <button key={genre._id}>{genre.name}</button>
       ))}
     </div>
   );
