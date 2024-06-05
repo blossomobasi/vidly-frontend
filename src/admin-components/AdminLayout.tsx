20366bcb0f95af7418697603df200082483aab59
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 14:51:32 +0100

    refactor: Create AdminLayout component for the admin section

---
+++ src/admin-components/AdminLayout.tsx
@@ -0,0 +1,26 @@
+import AdminHeader from "./AdminHeader";
+import AdminSidebar from "./AdminSidebar";
+import AdminRoute from "../components/AdminRoute";
+import { useUser } from "../hooks/auth/useUser";
+import styles from "./AdminLayout.module.css";
+
+function AdminLayout() {
+  const { user } = useUser();
+  return (
+    <div className={styles.container}>
+      <AdminHeader />
+      <AdminSidebar />
+
+      <main>
+        <h1>AdminRoute</h1>
+        <p>Welcome Admin {user?.firstName}</p>
+
+        <p>Data that should only be visible to an ADMIN</p>
+
+        <AdminRoute />
+      </main>
+    </div>
+  );
+}
+
+export default AdminLayout;
