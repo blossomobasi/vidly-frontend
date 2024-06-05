eb9e69491ab56bdbaf18cd2d64c6436336134891
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 20:46:30 +0100

    refactor: Update import path for AddMovie and DeleteMovie components in AdminMovie page

--- src/pages/admin/AdminMovie.tsx
+++ src/pages/admin/AdminMovie.tsx
@@ -1,10 +1,13 @@
-import AddMovie from "../../components/AddMovie";
+import AddMovie from "../../admin-components/AddMovie";
+import DeleteMovie from "../../admin-components/DeleteMovie";
 
 function AdminMovie() {
   return (
     <div>
       <h2>Operations on Movies</h2>
+
       <AddMovie />
+      <DeleteMovie />
     </div>
   );
 }
