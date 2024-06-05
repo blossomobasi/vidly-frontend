bc39c3f7bfcb42660223210648f40e28721f4f57
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 20:45:49 +0100

    refactor: Create useDeleteMovie hook for handling movie deletion

---
+++ src/hooks/movies/useDeleteMovie.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteMovie as deleteMovieApi } from "../../services/movies/deleteMovie";
+import toast from "react-hot-toast";
+import { MovieResponse } from "../../types/movies";
+
+export function useDeleteMovie() {
+  const queryClient = useQueryClient();
+  const { mutate: deleteMovie, isPending: isDeleting } = useMutation<
+    MovieResponse,
+    Error,
+    string
+  >({
+    mutationFn: deleteMovieApi,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["movies"],
+      });
+      toast.success("Movie deleted successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { isDeleting, deleteMovie };
+}
