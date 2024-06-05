a1b9a3d1deca7f14990004e344814ae3f53401c2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 14:52:40 +0100

    refactor: Remove AdminGenre component from AdminRoute and delete AdminGenre.tsx

--- src/components/AdminRoute.tsx
+++ src/components/AdminRoute.tsx
@@ -1,20 +1,7 @@
-import AdminGenre from "../admin-components/AdminGenre";
-import AdminMovie from "../admin-components/AdminMovie";
-import { useUser } from "../hooks/auth/useUser";
+import { Outlet } from "react-router-dom";
 
 function AdminRoute() {
-  const { user } = useUser();
-  return (
-    <div>
-      <h1>AdminRoute</h1>
-      <p>Welcome Admin {user?.firstName}</p>
-
-      <p>Data that should only be visible to an ADMIN</p>
-
-      <AdminGenre />
-      <AdminMovie />
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default AdminRoute;
