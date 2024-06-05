63af756e4a26f5a1db4ca108d04bcd1565da15fa
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 14:52:07 +0100

    chore: Add AdminSidebar component for the admin section and AdminSidebar module for styling

---
+++ src/admin-components/AdminSidebar.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import styles from "./AdminSidebar.module.css";
+
+function AdminSidebar() {
+  const baseLink = "/admin-dashboard";
+  const sidbarLinks = [
+    { name: "home", href: `${baseLink}` },
+    { name: "genres", href: `${baseLink}/genres` },
+    { name: "movies", href: `${baseLink}/movies` },
+    { name: "users", href: `${baseLink}/users` },
+  ];
+
+  return (
+    <div className={styles.sidebarContainer}>
+      <ul className={styles.ul}>
+        {sidbarLinks.map((link) => (
+          <li key={link.name}>
+            <NavLink to={link.href}>{link.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AdminSidebar;
