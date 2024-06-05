ccbc0a8dbaf816192d947d2499b951fbe4f327ea
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-05 14:53:51 +0100

    refactor: Create AdminMovie component for managing movies in the admin section

---
+++ src/pages/admin/AdminMovie.tsx
@@ -0,0 +1,12 @@
+import AddMovie from "../../components/AddMovie";
+
+function AdminMovie() {
+  return (
+    <div>
+      <h2>Operations on Movies</h2>
+      <AddMovie />
+    </div>
+  );
+}
+
+export default AdminMovie;
