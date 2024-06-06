78d74457e8482a8c48596455e404d4ea48e6a801
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-06 10:31:58 +0100

    refactor: Add dateFormatter utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,30 @@
 import { jwtDecode } from "jwt-decode";
 
 export function decodeToken(token: string) {
-  if (!token) return null;
-  try {
-    return jwtDecode(token);
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    if (!token) return null;
+    try {
+        return jwtDecode(token);
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
 
 export function getToken() {
-  const token = localStorage.getItem("token");
-  return token;
+    const token = localStorage.getItem("token");
+    return token;
+}
+
+export function dateFormatter(dateString: string) {
+    const date = new Date(dateString);
+
+    return date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        timeZoneName: "short",
+    });
 }
