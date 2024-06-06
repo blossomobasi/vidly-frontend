5497bfa40ba3ad9c6ff90863d134b6bbe1aa9760
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-06 10:30:40 +0100

    feat: Create AdminUser page for managing user data

---
+++ src/pages/admin/AdminUser.tsx
@@ -0,0 +1,13 @@
+import AllUsers from "../../admin-components/AllUsers";
+
+function AdminUser() {
+    return (
+        <div>
+            <h3>Operations on Users</h3>
+
+            <AllUsers />
+        </div>
+    );
+}
+
+export default AdminUser;
