357b7dee4df8a844077f6d01586879906b5a03ea
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-14 13:29:03 +0100

    Add AdminCustomer componenet

---
+++ src/pages/admin/AdminCustomer.tsx
@@ -0,0 +1,13 @@
+import AllCustomers from "../../admin-components/AllCustomers";
+
+const AdminCustomer = () => {
+    return (
+        <div>
+            <h3>Operations on Customers</h3>
+
+            <AllCustomers />
+        </div>
+    );
+};
+
+export default AdminCustomer;
