82e680fc23df730ba776e53356fb5d1ed63b8479
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-18 14:36:24 +0100

    Update: Reload page when genre has been changed

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useGenres } from "../hooks/genres/useGenres";
 import { Genre } from "../types/genre";
 
@@ -7,9 +7,12 @@ function Genres() {
     const { data: genres, isLoading, error } = useGenres();
 
     const genreIdFromLocalStorage = localStorage.getItem("selectedGenre");
+
     const handleGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedGenre(e.target.value);
         localStorage.setItem("selectedGenre", e.target.value);
+
+        window.location.reload();
     };
 
     return (
@@ -34,3 +37,35 @@ function Genres() {
 }
 
 export default Genres;
+
+// Using the useRef();
+
+// import React, { useState } from "react";
+// import { useGenres } from "../hooks/genres/useGenres";
+// import { Genre } from "../types/genre";
+
+// interface GenresProps {
+//     selectedGenre: string | null;
+//     onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+// }
+
+// function Genres({ selectedGenre, onChange }: GenresProps) {
+//     const { data: genres, isLoading, error } = useGenres();
+
+//     return (
+//         <div>
+//             {isLoading && <div>Loading...</div>}
+//             {error && <div>{error.message}</div>}
+//             <select name="genres" id="genres" value={selectedGenre || ""} onChange={onChange}>
+//                 <option value="">Select a genre</option>
+//                 {genres?.map((genre: Genre) => (
+//                     <option key={genre._id} value={genre._id}>
+//                         {genre.name}
+//                     </option>
+//                 ))}
+//             </select>
+//         </div>
+//     );
+// }
+
+// export default Genres;
